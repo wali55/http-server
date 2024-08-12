6c5141b7c7b28ea8274f8136b512043c7412fdc1
wali55 <walisantunu@gmail.com>
2024-08-12 08:06:37 +0600

    create server

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Create and expose and HTTP server
+ * 1. npm init -y (can see package.json file)
+ * 2. create index.js
+ * 3. require express
+ * 4. npm i express
+ * 5. const app = express()
+ * 6. Assignment create a todo app that lets users todo on the server
+ */
+
+/**
+ * Create and expose and HTTP server
+ * 1. call the express function it will give us the app object
+ * 2. when I want to create route I can call app.get() for example first parameter is the route and second parameter is the callback function, the cb function will have 2 parameters, request and response.
+ * 3. request contains all the details related to the request like: request header, request body, request method, query parameters
+ * 4. we can do some ml logic here and server will give a response back
+ * 5. I can also use res.json() to send and json as a response.
+ * 6. why do we need port? if we have multiple application on the one server, we can use different port for each application.
+ */
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    name: 'wali',
+    age: 32
+  })
+});
+
+app.post("/conversations", (req, res) => {
+  console.log(req.body);
+  res.json({
+    msg: "2 + 2 = 4"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}!`);
+});
